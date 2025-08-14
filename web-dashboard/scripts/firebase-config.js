e5b67295b078203e00bc68896db90fe2148eb7bb
Mar Kevin P. Alcantara <markevinalcantara40@gmail.com>
2025-08-15 00:26:13 +0800

    done for now

--- web-dashboard/scripts/firebase-config.js
+++ web-dashboard/scripts/firebase-config.js
@@ -13,6 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { app, auth };
+export const auth = getAuth(app);
